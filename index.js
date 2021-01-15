bfd3666f3ec27d8d843588290c32ce27abe5baa3
Ana <anaalamed.gmail.com>
2021-01-15 20:50:34 +0200

    end lesson

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-require('./todos-cmd');
+const runCommand = require('./todos-cmd');
 console.log('todos app is ready!');
 
-const [a,b,action,data] = process.argv;
+// const [a, b, action, data] = process.argv;
+
+runCommand(process.argv);
--- todos-cmd.js
+++ todos-cmd.js
@@ -1,3 +1,6 @@
 console.log('comands loaded')
 require('./todo-model')
 
+module.exports = function runCommand([a,b,action, data]) {
+
+}
